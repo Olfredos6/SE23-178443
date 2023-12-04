dcd4b8197d84d4d6dcb3573da20fc2a54ea0e314
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2023-12-04 18:15:56 +0200

    Add request methods handling

--- portfolio/hello_app/urls.py
+++ portfolio/hello_app/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("login/", views.login),
     path("hello/<slug:name>", views.hello),
+    path("hello-json", views.hello_json),
     path("hi/<str:name>/<str:last_name>", views.hi),  # hi(name)
     path("", views.index),
 ]
--- portfolio/hello_app/views.py
+++ portfolio/hello_app/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def hello(request, name):
@@ -6,10 +7,28 @@ def hello(request, name):
 
 
 def hi(request, name, last_name):
-    print("~~~~>", request)
-    print("~~~~>", name)
+    print("~~~~>", dir(request))
+    print("~~~~>", request.GET["search"])
     return HttpResponse(f"Salut, {name} {last_name}!")
 
 
 def index(request):
     return HttpResponse(f"<h1>Bienvenue!</h2>")
+
+
+def hello_json(request):
+    return JsonResponse({"data": "Bonjour!"})
+
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":  # [DELETE, POST, UPDATE]
+        return HttpResponse("Fromulaire soumis")
+
+    return HttpResponse(
+        """<form action='/hello-app/login/' method='POST'>
+            <input type='password' name='password' />
+            <input type='submit' value='Ok'>
+        </form>
+        """
+    )
