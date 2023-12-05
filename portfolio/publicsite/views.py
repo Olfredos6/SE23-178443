38eb382a8f5d24448a7702453b2db642cf4c53de
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2023-12-05 13:24:38 +0200

    Response context

--- portfolio/publicsite/views.py
+++ portfolio/publicsite/views.py
@@ -1,14 +1,27 @@
+import json
+from pathlib import Path
+
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
+
+EXPERIENCE_DATA_FILE = Path(
+    settings.BASE_DIR / "publicsite" / "data" / "experience.json"
+)
 
 
 def index(request):
     # return HttpResponse("Index|A proopos")
-    return render(request, "publicsite/about.html")
+    return render(request, "publicsite/about.html", {})
 
 
 def experience(request):
-    return render(request, "publicsite/experience.html")
+    with open(EXPERIENCE_DATA_FILE, "r", encoding="utf-8") as f:
+        experience_data = json.loads(f.read())["experiences"]
+
+    return render(
+        request, "publicsite/experience.html", {"maman": experience_data, "manuel": 46}
+    )
 
 
 def blog(request):
